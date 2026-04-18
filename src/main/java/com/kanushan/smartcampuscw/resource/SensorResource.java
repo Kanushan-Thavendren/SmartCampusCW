e2539e4ecb14237922b828465bae0dcab92df655
Kanushan <kanushanthavendren@gmail.com>
2026-04-18 10:08:12 +0530

    Add POST sensors with room validation

---
+++ src/main/java/com/kanushan/smartcampuscw/resource/SensorResource.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.kanushan.smartcampuscw.resource;
+
+import com.kanushan.smartcampuscw.dao.SensorDAO;
+import com.kanushan.smartcampuscw.dao.RoomDAO;
+import com.kanushan.smartcampuscw.model.Sensor;
+import com.kanushan.smartcampuscw.model.Room;
+
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+@Path("/sensors")
+public class SensorResource {
+
+    private SensorDAO sensorDAO = new SensorDAO();
+    private RoomDAO roomDAO = new RoomDAO();
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response addSensor(Sensor sensor) {
+
+        Room room = roomDAO.getRoomById(sensor.getRoomId());
+
+        if (room == null) {
+            return Response.status(Response.Status.BAD_REQUEST)
+                    .entity("Room does not exist")
+                    .build();
+        }
+
+        sensorDAO.addSensor(sensor);
+
+        room.getSensorIds().add(sensor.getId());
+
+        return Response.status(Response.Status.CREATED)
+                .entity(sensor)
+                .build();
+    }
+}
