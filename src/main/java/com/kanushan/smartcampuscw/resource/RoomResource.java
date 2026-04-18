6f405e8681681f8572019a6ef2838f306b52192e
Kanushan <kanushanthavendren@gmail.com>
2026-04-18 09:21:09 +0530

    Add GET room by ID endpoint

--- src/main/java/com/kanushan/smartcampuscw/resource/RoomResource.java
+++ src/main/java/com/kanushan/smartcampuscw/resource/RoomResource.java
@@ -14,6 +14,7 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.PathParam;
 
 @Path("/rooms")
 public class RoomResource {
@@ -34,5 +35,12 @@ public class RoomResource {
         return Response.status(Response.Status.CREATED)
                 .entity(room)
                 .build();
+    }
+    
+    @GET
+    @Path("/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Room getRoomById(@PathParam("id") String id) {
+        return roomDAO.getRoomById(id);
     }
 }
